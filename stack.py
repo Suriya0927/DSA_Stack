7c0149fb1d1278768e9f5d2b865e0d8f6cedcf0c
suriya0927 <suriyas2005@gmail.com>
2023-10-19 17:43:52 +0530

    Added option to set size for list
    Now we can set size limit for lists. It gives warning message, if it reach the end and it gives warning if we try to pop on empty list

--- stack.py
+++ stack.py
@@ -3,17 +3,23 @@ class myStack:
         self.index = -1
         self.stack = []
 
-    def push(self, ele):
+    def push(self, ele, limit):
         self.index += 1
-        self.stack.append(ele)
-        return ele,' added into stack'
-        
+        if limit > self.index:
+            self.stack.append(ele)
+            return ele,' added into stack'
+        else:
+            self.index -= 1
+            return f'Reached list size {limit}. No more push allowed'
         
     def pop(self):
-        self.temp = self.stack[self.index]
-        self.index -= 1
-        self.stack.pop()
-        return self.temp,' removed from stack'
+        if self.index == -1:
+            return 'List is EMPTY. We cannot pop'
+        else:
+            self.temp = self.stack[self.index]
+            self.index -= 1
+            self.stack.pop()
+            return self.temp,' removed from stack'
 
     def peek(self):
         return 'TOP STACK IS: ',self.stack[self.index]
@@ -27,14 +33,15 @@ class myStack:
 
 user_input=''
 x=myStack()
-
+limit = int(input('Enter list size: '))
 while user_input != '0':
+    print('')
     print('1.PUSH | 2.POP | 3.PEEK | 4.isEMPTY | 5.SHOW LIST | 0.EXIT')
     user_input=input('Enter: ')
 
     if user_input == '1': #PUSH
         push_input=input('enter push value: ')
-        print(x.push(push_input))
+        print(x.push(push_input, limit))
         print('STACK: ', x.stack)
 
     elif user_input == '2': #POP
